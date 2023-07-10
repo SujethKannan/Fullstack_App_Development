63c055799f2e0d28474e7a3531eaa70fb0275ced
SujethKannan <sujethkannan98@gmail.com>
2023-07-10 09:29:42 +0530

    commit-10

--- students_management_app/src/controllers/staffs.js
+++ students_management_app/src/controllers/staffs.js
@@ -1,4 +1,5 @@
-const models= require('../models/staffs');
+const staffsmodels= require('../models/staffs');
+const settingsmodels= require('../models/settings');
 const constants = require('../config/constants')
 const {validationResult} = require('express-validator')
 
@@ -7,17 +8,20 @@ module.exports={
     getIndex:function(request,response){
        return  response.render('staffs/index',{errors:{}})
     },
-    getAddStaff:function(request,response){
-        return response.render('staffs/addStaff',{errors:{}})
+    getAddStaff: async function(request,response){
+        const departmentData = await getAllDepartments();
+        return response.render('staffs/addStaff',{errors:{}, departmentData:departmentData })
     }, 
     addStaff: async function(request,response){
         try{
+            const departmentData = await getAllDepartments();
             const validationErrors = validationResult(request);
             // console.log('validationErrors >>>',validationErrors);
 
             if(!validationErrors.isEmpty()){
                 return response.render('staffs/addStaff',{
-                    errors : validationErrors.mapped()
+                    errors : validationErrors.mapped(), 
+                    departmentData:departmentData
                 })
             }
             const name = request.body.name;
@@ -32,11 +36,11 @@ module.exports={
                 mobile_no:mobile_no
             }
 
-            const result = await models.insertStaff(data);
+            const result = await staffsmodels.insertStaff(data);
             if(result===constants.resultFlag.success){
-               return  response.render('staffs/index',{errors:{opsError:'Staff Added Successfully'}})
+               return  response.render('staffs/addStaff',{errors:{opsError:'Staff Added Successfully'}, departmentData:departmentData})
             }
-              return response.render('staffs/index',{errors:{opsError:'Something Went wrong while adding staff'}})
+              return response.render('staffs/addStaff',{errors:{opsError:'Something Went wrong while adding staff'}, departmentData:departmentData})
 
         }catch(error){
 
@@ -48,6 +52,7 @@ module.exports={
 
     searchStaff : async function(request,response){
         try {
+            const departmentData = await getAllDepartments();
             const validationErrors = validationResult(request);
             // console.log('validationErrors >>>',validationErrors);
             if(!validationErrors.isEmpty()){
@@ -55,13 +60,13 @@ module.exports={
                     errors : validationErrors.mapped()})
             }
             const staffId = request.body.staff_id;
-            const staffData = await models.getStaffById(staffId)
+            const staffData = await staffsmodels.getStaffById(staffId)
          if(!staffData){
             return response.render('staffs/index',{
                 errors : {no_Data:'Staff data does not exists'}})
 
         }
-        return response.render('staffs/searchstaff',{data: staffData, errors:{}})
+        return response.render('staffs/searchstaff',{data: staffData, errors:{}, departmentData:departmentData})
     }
         catch (error) {
             console.log('[searchStaff Controller] error:',error);
@@ -71,14 +76,16 @@ module.exports={
 
     updateStaff : async function(request,response){
         try {
+            const departmentData = await getAllDepartments();
             const id = request.body.id;
-            const staffData = await models.getStaffById(id);
+            const staffData = await staffsmodels.getStaffById(id);
             const validationErrors = validationResult(request);
             // console.log('validationErrors >>>',validationErrors);
             if(!validationErrors.isEmpty()){
                 return response.render('staffs/searchstaff',{
                     errors : validationErrors.mapped(),
-                    data : staffData
+                    data : staffData,
+                    departmentData:departmentData
                 })
             }
 
@@ -95,17 +102,19 @@ module.exports={
                 mobile_no:mobile_no,
                 email:email
             }
-        const updatedresult = await models.updateStaffById(data,id);
+        const updatedresult = await staffsmodels.updateStaffById(data,id);
         if(!updatedresult === constants.resultFlag.error){
             return response.render('staffs/searchstaff',{
                 errors : {updateError: 'unable to update data'},
-                data : staffData
+                data : staffData,
+                departmentData:departmentData
             })
         }
-        const updatedstaffData = await models.getStaffById(id);        
+        const updatedstaffData = await staffsmodels.getStaffById(id);        
         return response.render('staffs/searchstaff',{
             errors : {successmsg : 'Data updated successfully'},
-            data : updatedstaffData
+            data : updatedstaffData,
+            departmentData:departmentData
         })
 
         } catch (error) {
@@ -116,7 +125,7 @@ module.exports={
     deleteStaff : async function(request,response){
         try {
             const staffId = request.params.staff_id;
-            const result = await models.deleteStaffById(staffId);
+            const result = await staffsmodels.deleteStaffById(staffId);
             if(result === constants.resultFlag.error){
                 return response.render('staffs/index',{errors:{opsError:'Something Went wrong while deleting staff'}})
             }
@@ -128,7 +137,7 @@ module.exports={
     },
     getStaffsDetails : async function(request,response){
         try {
-            const data = await models.getStaffs();
+            const data = await staffsmodels.getStaffs();
             return response.render('staffs/details-page',{data:data});
         } catch (error) {
             console.log('[getStaffsDetails Controller] error:',error);
@@ -137,3 +146,12 @@ module.exports={
         }
     }
 }
+
+const getAllDepartments = async ()=>{
+    try {
+        return await settingsmodels.getDepartment();
+    } catch (error) {
+        console.log('[getAllDepartments] error:',error);
+        return null;        
+    }
+ }
